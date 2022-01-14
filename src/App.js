854d0074deb561cc9361e79ea708adbfc1d0ee98
my0990 <my0990@naver.com>
2022-01-14 17:24:54 +0900

    third commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {useState} from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
 import ImgInsert from './components/imgInsert';
+import PlayScreen from './components/play_screen';
 
 function App() {
   
@@ -10,11 +11,15 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/imgInsert" element={<ImgInsert />} />
+        <Route path="imgInsert" exact element={<ImgInsert />} />
+        <Route path="playScreen" exact element={<PlayScreen />} />
       </Routes>
       app페이지
       <Link to='/imgInsert'>
         <div>imgInsert</div>
+      </Link>
+      <Link to='/playScreen'>
+        <div>play_screen</div>
       </Link>
     </div>
   );
